453a60969c1f125a97f8833491fcc088fa9bdc7a
Ford Parsons <github@snosrap.com>
2012-09-06 16:19:02 -0500

    Initial Commit

---
+++ WiiPair/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WiiPair
+//
+//  Created by Ford Parsons on 9/6/12.
+//  Copyright (c) 2012 Ford Parsons. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
